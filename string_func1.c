f21fc1217236ac7f924d10be25a8996aabd78052
SlammyTee <126764311+SlammyTee@users.noreply.github.com>
2023-09-20 17:56:22 +0100

    Update string_func1.c

--- string_func1.c
+++ string_func1.c
@@ -14,3 +14,20 @@ int str_len(char *str)
         }
         return (i);
 }
+/**
+ * str_cpy - function to copy the string pointed to by src
+ * to the string pointed to by dest.
+ * @dest: A pointer to destination of string
+ * @src: A pointer to source string to copy from
+ * Return: pointer to destination string
+ */
+char *str_cpy(char *dest, char *src)
+{
+	char *aux = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
+}
+
+
